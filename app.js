554d69fda8ea3aa0405183dff5579d9fbffbef06
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 20:32:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b = 49;
+if (a === 5) {
+    console.log("Sayın 5e eşit");
+}
+else {
+    console.log("Sayın 1e eşit değil");
+}
+//  JAVASCRİPTLE AYNI
+a > b ? console.log("A büyük b") : console.log("B büyük a");
---
+++ app.ts
@@ -0,0 +1,10 @@
+let a:number = 5;
+let b : number = 49;
+if (a === 5) {
+    console.log("Sayın 5e eşit")
+}
+else {
+    console.log("Sayın 1e eşit değil")
+}
+//  JAVASCRİPTLE AYNI
+a>b ? console.log("A büyük b") : console.log("B büyük a")
